e132afbd5f2b42b0f312e2201c3d05e71df029fb
Immaculate Munde <immaculatemunde1@gmail.com>
2025-10-20 01:53:07 +0300

    feat: add SQL script to show full description of books table without using DESCRIBE

---
+++ task_4.sql
@@ -0,0 +1,14 @@
+-- Select the alx_book_store database
+USE alx_book_store;
+
+-- Display full description of the 'books' table
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'books';
